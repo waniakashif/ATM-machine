25944fb0b9cd3785a985e1ba961cb8488fe34dda
unknown <kashifwania8@gmail.com>
2024-04-15 17:01:09 +0500

    push my atm machine to github

---
+++ main.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let myBalance = 10000; //Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["Withdraw", "check Balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("Your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "check Balance") {
+        console.log("yourbalance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect pin number");
+}
